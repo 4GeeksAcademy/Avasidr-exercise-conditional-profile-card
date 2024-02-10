3b50f2db25f16a0db1adfec193a4940e8b2eb56b
Ainoha Franco <76441303+Avasidr@users.noreply.github.com>
2024-02-10 17:52:32 +0000

    Proyeto finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -40,11 +40,27 @@ function render(variables = {}) {
           <h3>${variables.city == null ? "" : variables.city} ${
     variables.country == null ? "" : variables.country
   }</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter == null
+                ? "https://twitter.com"
+                : `https://twitter.com/${variables.twitter}`
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github == null
+                ? "https://github.com"
+                : `https://github.com/${variables.github}`
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin == null
+                ? "https://linkedin.com"
+                : `https://linkedin.com/${variables.linkedin}`
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram == null
+                ? "https://instagram.com"
+                : `https://instagram.com/${variables.instagram}`
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
